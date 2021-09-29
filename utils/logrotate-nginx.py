68a5dc4d690edaf4b0193b0bdb994c4143ed93e5
离线请留言 <347061329@qq.com>
2021-09-29 19:03:18 +0800

    auto

---
+++ utils/logrotate-nginx.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import datetime,os,sys,shutil
+
+log_path = '/app/nginx/logs/'
+log_file = 'www_access.log'
+
+yesterday = (datetime.datetime.now() - datetime.timedelta(days = 1))
+
+try:
+    os.makedirs(log_path + yesterday.strftime('%Y') + os.sep + \
+                yesterday.strftime('%m'))
+
+except OSError,e:
+    print
+    print e
+    sys.exit()
+
+
+shutil.move(log_path + log_file,log_path \
+            + yesterday.strftime('%Y') + os.sep \
+            + yesterday.strftime('%m') + os.sep \
+            + log_file + '_' + yesterday.strftime('%Y%m%d') + '.log')
+
+
+os.popen("sudo kill -USR1 `cat /app/nginx/logs/nginx.pid`")
